9f61cb04eaf3d534bd8098967f8c482474b355c1
Jeroen Doggen <jeroendoggen@gmail.com>
2020-10-16 14:52:56 +0200

    jpegcleaner.py: move bad files to ./temp, validate with jpegdupes, support filenames with spaces

--- jpegcleaner/jpegcleaner.py
+++ jpegcleaner/jpegcleaner.py
@@ -1,14 +1,14 @@
+#!/usr/bin/env python3
+
 """
-    jpegcleaner.py: tool to clean up folders containing invalid jpeg images
+    jpegcleaner.py: tool to clean up folders containing invalid and duplicate jpeg images
     jpegcleaner.py is copyright 2020 Jeroen Doggen.
 """
 
 
-#!/usr/bin/env python3
-
-
 import os
 import argparse
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument("folder", help="the folder to clean")
@@ -27,12 +27,15 @@ for subdir, dirs, files in os.walk(args.folder):
 
         if filepath.endswith(".jpeg") or filename.endswith(".JPEG") or filename.endswith(".jpg") or filename.endswith(".JPG"):
             #print("JPEG file: " + filename)
-            retVal = os.system('jpeginfo -c ' + filepath) # returns the exit status
+            retVal = os.system('jpeginfo -c ' + "\""+ filepath + "\"") # returns the exit status
         
             if(0 != retVal):
-                os.remove(filepath)
+                #os.remove(filepath)
+                shutil.move(filepath, './temp/')
                 print("Removed invalid JPEG file: " + filepath)
-            
+
+print("\nRemoving duplicate files with jpegdupes")
+retVal = os.system('jpegdupes -d -a ' + args.folder)             
 
 print("\nStorage space for this folder after cleanup:")
 retVal = os.system('du -sh ' + args.folder) 
